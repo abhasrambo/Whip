4f7da4c9dc84350982e6ee40d3811a86e3d4dd37
Abhas Kumar <abhasrambo@mail.fresnostate.edu>
2020-04-15 15:48:06 -0700

    Initial Commit

---
+++ Whip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whip
+//
+//  Created by Abhas Kumar on 4/15/20.
+//  Copyright © 2020 Abhas Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
